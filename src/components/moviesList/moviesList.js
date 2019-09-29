a457bb5ea4003641058b51aaf473129f1e7aecc8
Ritesh Kumar <riteshkumar@Saviynts-MacBook-Pro.local>
2019-09-30 01:42:34 +0530

    Implemented Search functiolanity

--- src/components/moviesList/moviesList.js
+++ src/components/moviesList/moviesList.js
@@ -1,27 +1,104 @@
-import React, {Component, useEffect} from 'react';
+import React, { Component, useEffect } from 'react';
 import './moviesList.css';
 import image1 from '../../assets/img-1.jpg';
 import { connect } from 'react-redux';
+import SearchIcon from '@material-ui/icons/Search';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import InputBase from '@material-ui/core/InputBase';
+
 //import { loadMovieList } from '../../actions/action';
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+}));
 
 class MoviesList extends Component {
-  componentDidMount () {
+  constructor(props) {
+    super(props);
+    console.log(props);
+    this.state = {
+      movieList: this.props.movieList,
+      byName: '',
+      byYear: '',
+    };
+  }
+
+  componentDidMount() {
     this.props.getImageList();
   }
+  componentDidUpdate() {
+    if (this.state.movieList === null) {
+      this.setState({
+        movieList: this.props.movieList
+      });
+    }
+  }
+
+  reset = e => {
+    this.setState({
+      movieList: this.props.movieList,
+      byName: '',
+      byYear: '',
+    });
+
+  }
+  searchByName = prop => e => {
+    this.setState({
+      movieList: this.props.movieList.filter(item => {
+        let name = this.state.byName;
+        let year = this.state.byYear;
+        if (prop === 'byName') {
+          name = e.target.value;
+        }
+        if (prop === 'byYear') {
+          year = e.target.value;
+        }
+        return item.genre.toLowerCase().indexOf(name.toLowerCase()) !== -1 && item.productionYear.toString().indexOf(year) !== -1;
+      }),
+      [prop]: e.target.value
+    });
+
+  }
+
   render() {
-    const data = this.props.movieList || [];
+    const data = this.state.movieList || [];
     let imageArray = [];
     data.forEach(element => {
-      imageArray.push(<li className="grid-contents"><img className="image-style" src={element.image} alt="movies" /><div>{element.name}</div> </li>);
+      imageArray.push(<li key={element.image} className="grid-contents"><img className="image-style" src={element.image} alt="movies" /><div>{element.name}</div> </li>);
     });
     return (
       <div>
-      <header className="App-header">Movies-Gallery     </header>
-
-      <ul className="mainlist">
-        {imageArray}
-      </ul>
-  </div>
+        <header className="App-header">
+          <div className="searchMainSection">
+            <div className = "headerSectionContents">
+              <label>Movies List</label>
+            </div>
+            <div className = "headerSectionContents">
+              <input type="text" className="SearchBoxSection" value={this.state.byName} onChange={this.searchByName('byName')} placeholder = "Search By Genre"></input>
+            </div>
+            <div className = "headerSectionContents">
+              <input type="text" className="SearchBoxSection" value={this.state.byYear} onChange={this.searchByName('byYear')} placeholder = "Search By Year"></input>
+            </div>
+            <div className = "headerSectionContents">
+              <button onClick={this.reset} className="clear-search-btn"> Clear Search</button>
+            </div>
+          </div>
+        </header>
+        <ul className="mainList">
+          {imageArray}
+        </ul>
+      </div>
     );
   }
 }
